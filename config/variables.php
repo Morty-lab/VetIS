ca23b9d756b1a51978a51b91c61013aab2ed5eab
kent <aniejosephcabahug@gmail.com>
2024-03-04 10:42:08 +0800

    Initial commit

---
+++ config/variables.php
@@ -0,0 +1,11 @@
+<?php
+
+return[
+    "creatorName" => "PowerPuffDevs",
+    "systemName" => "VetIS",
+    "systemSuffix" => "VetIS - Veterinary Information System",
+    "systemVersion" => "1.0 Beta",
+    "systemFree" => false,
+    "systemDescription" => "VetIS – is the most userfriendly Veterinary Clinic Information System.",
+    "templateKeyword" => "veterinary clinic, veterinary, veterinarian, veterinary medicine",
+];
