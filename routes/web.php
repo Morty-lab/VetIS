a855c50bb6d8205bf68709a5f9517853489ad377
morty <aniejosephcabahug@gmail.com>
2024-03-28 10:51:00 +0800

    Added models and migrations for partial system

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,15 @@ Route::get('/', function () {
     return view('auth.login');
 });
 
+Route::get('/test', function () {
+
+    $response = "test suceeded";
+
+    return $response;
+});
+
+
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -26,6 +35,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
 });
 
 require __DIR__.'/auth.php';
