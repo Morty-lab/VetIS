cb0470b04899500342997d9da581db5f2ea71466
kent <aniejosephcabahug@gmail.com>
2024-04-05 13:26:27 +0800

    Committing local changes to routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/addpet', function () {
+    return view('pets.add');
+});
+
 require __DIR__.'/auth.php';
