d1661025d9d63ba16c15b69bfc0ae7c4fb9cdc82
kent <princeinventorevltn89@gmail.com>
2024-04-08 00:47:34 +0800

    fixed asset errors

--- resources/views/doctors/add.blade.php
+++ resources/views/doctors/add.blade.php
@@ -45,7 +45,7 @@
                                 <label class="small mb-1" for="inputFirstName">First name</label>
                                 <input class="form-control @error('firstname') is-invalid @enderror" id="inputFirstName" name="firstname" type="text" placeholder="First Name" />
                                 @error('firstname')
-                                    <div class="invalid-feedback">{{ $message }}</div>
+                                <div class="invalid-feedback">{{ $message }}</div>
                                 @enderror
                             </div>
                             <!-- Form Group (last name)-->
@@ -53,7 +53,7 @@
                                 <label class="small mb-1" for="inputLastName">Last name</label>
                                 <input class="form-control @error('lastname') is-invalid @enderror" id="inputLastName" name="lastname" type="text" placeholder="Last Name" />
                                 @error('lastname')
-                                    <div class="invalid-feedback">{{ $message }}</div>
+                                <div class="invalid-feedback">{{ $message }}</div>
                                 @enderror
                             </div>
                         </div>
@@ -62,7 +62,7 @@
                             <label class="small mb-1" for="inputEmailAddress">Email address</label>
                             <input class="form-control @error('email') is-invalid @enderror" id="inputEmailAddress" name="email" type="email" placeholder="Email Address" value="" />
                             @error('email')
-                                <div class="invalid-feedback">{{ $message }}</div>
+                            <div class="invalid-feedback">{{ $message }}</div>
                             @enderror
                         </div>
                         <!-- Add similar error handling for other fields -->
@@ -71,7 +71,7 @@
                             <label class="small mb-1" for="inputAddress">Address</label>
                             <input class="form-control @error('address') is-invalid @enderror" id="inputAddress" name="address" type="text" placeholder="Address" value="" />
                             @error('address')
-                                <div class="invalid-feedback">{{ $message }}</div>
+                            <div class="invalid-feedback">{{ $message }}</div>
                             @enderror
                         </div>
                         <!-- Form Row-->
@@ -81,7 +81,7 @@
                                 <label class="small mb-1" for="inputPhone">Phone number</label>
                                 <input class="form-control @error('phone_number') is-invalid @enderror" id="inputPhone" name="phone_number" type="tel" placeholder="Phone Number" value="" />
                                 @error('phone_number')
-                                    <div class="invalid-feedback">{{ $message }}</div>
+                                <div class="invalid-feedback">{{ $message }}</div>
                                 @enderror
                             </div>
                             <!-- Form Group (birthday)-->
@@ -89,7 +89,7 @@
                                 <label class="small mb-1" for="inputBirthday">Birthday</label>
                                 <input class="form-control @error('birthday') is-invalid @enderror" id="inputBirthday" name="birthday" type="date" placeholder="MM/DD/YYYY" value="" />
                                 @error('birthday')
-                                    <div class="invalid-feedback">{{ $message }}</div>
+                                <div class="invalid-feedback">{{ $message }}</div>
                                 @enderror
                             </div>
                         </div>
@@ -98,7 +98,7 @@
                             <label class="small mb-1" for="inputPosition">Position</label>
                             <input class="form-control @error('position') is-invalid @enderror" id="inputPosition" name="position" type="text" placeholder="Position" value="" />
                             @error('position')
-                                <div class="invalid-feedback">{{ $message }}</div>
+                            <div class="invalid-feedback">{{ $message }}</div>
                             @enderror
                         </div>
                         <!-- Form Group (username)-->
@@ -106,7 +106,7 @@
                             <label class="small mb-1" for="inputUsername">Username</label>
                             <input class="form-control @error('username') is-invalid @enderror" id="inputUsername" name="username" type="text" placeholder="Username" value="" />
                             @error('username')
-                                <div class="invalid-feedback">{{ $message }}</div>
+                            <div class="invalid-feedback">{{ $message }}</div>
                             @enderror
                         </div>
                         <!-- Form Row-->
@@ -116,7 +116,7 @@
                                 <label class="small mb-1" for="inputPassword">Password</label>
                                 <input class="form-control @error('password') is-invalid @enderror" id="inputPassword" name="password" type="password" placeholder="Password" value="" />
                                 @error('password')
-                                    <div class="invalid-feedback">{{ $message }}</div>
+                                <div class="invalid-feedback">{{ $message }}</div>
                                 @enderror
                             </div>
                             <!-- Form Group (confirm password)-->
@@ -124,7 +124,7 @@
                                 <label class="small mb-1" for="confirmPassword">Confirm Password</label>
                                 <input class="form-control @error('password_confirmation') is-invalid @enderror" id="confirmPassword" name="password_confirmation" type="password" placeholder="Confirm Password" value="" />
                                 @error('password_confirmation')
-                                    <div class="invalid-feedback">{{ $message }}</div>
+                                <div class="invalid-feedback">{{ $message }}</div>
                                 @enderror
                             </div>
                         </div>
@@ -142,7 +142,7 @@
                 <div class="card-header">Profile Picture</div>
                 <div class="card-body text-center">
                     <!-- Profile picture image-->
-                    <img class="img-account-profile rounded-circle mb-2" src="assets/img/illustrations/profiles/profile-1.png" alt="" />
+                    <img class="img-account-profile rounded-circle mb-2" src="{{ asset('assets/img/illustrations/profiles/profile-1.png') }}" alt="" />
                     <!-- Profile picture help block-->
                     <div class="small font-italic text-muted mb-4">JPG or PNG no larger than 5 MB</div>
                     <!-- Profile picture upload button-->
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,11 +8,10 @@
     <meta name="description" content="" />
     <meta name="author" content="" />
     <title>Dashboard - VetIS</title>
-    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/litepicker/dist/css/litepicker.css" rel="stylesheet" />
-    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
-    <link rel="icon" type="image/x-icon" href="assets/img/favicon.png" />
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
+    <link rel="icon" type="image/x-icon" href="{{ asset('assets/img/favicon.png') }}" />
     @yield('styles')
     <script data-search-pseudo-elements="" defer="" src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.min.js" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.28.0/feather.min.js" crossorigin="anonymous"></script>
@@ -35,7 +34,7 @@
             <footer class="footer-admin mt-auto footer-light">
                 <div class="container-xl px-4">
                     <div class="row">
-                        <div class="col-md-6 small">Copyright © Your Website 2021</div>
+                        <div class="col-md-6 small">Copyright © VetIS 2024</div>
                         <div class="col-md-6 text-md-end small">
                             <a href="dashboard-1.html#!">Privacy Policy</a>
                             ·
@@ -48,14 +47,15 @@
     </div>
     <script data-cfasync="false" src="cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-    <script src="js/scripts.js"></script>
+    <script src="{{ asset('js/scripts.js') }}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js" crossorigin="anonymous"></script>
-    <script src="assets/demo/chart-area-demo.js"></script>
-    <script src="assets/demo/chart-bar-demo.js"></script>
+    <script src="{{ asset('assets/demo/chart-area-demo.js') }}"></script>
+    <script src="{{ asset('assets/demo/chart-bar-demo.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
-    <script src="js/datatables/datatables-simple-demo.js"></script>
+    <script src="{{ asset('js/datatables/datatables-simple-demo.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/litepicker/dist/bundle.js" crossorigin="anonymous"></script>
-    <script src="js/litepicker.js"></script>
+    <script src="{{ asset('js/litepicker.js') }}"></script>
+    @yield('scripts')
 
     <script>
         (function() {
@@ -96,7 +96,6 @@
         })();
     </script>
     <script defer src="https://static.cloudflareinsights.com/beacon.min.js/v652eace1692a40cfa3763df669d7439c1639079717194" integrity="sha512-Gi7xpJR8tSkrpF7aordPZQlW2DLtzUlZcumS8dMQjwDHEnw9I7ZLyiOj/6tZStRBGtGgN6ceN6cMH8z7etPGlw==" data-cf-beacon='{"rayId":"750b4b957f85073a","token":"6e2c2575ac8f44ed824cef7899ba8463","version":"2022.8.1","si":100}' crossorigin="anonymous"></script>
-    @yield('scripts')
 </body>
 
 </html>
