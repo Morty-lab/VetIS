58127a1e2a11b11bad85952655b63e141fe95817
morty <aniejosephcabahug@gmail.com>
2024-11-27 01:32:32 +0800

    Routes Clean up

--- app/Http/Controllers/PortalController.php
+++ app/Http/Controllers/PortalController.php
@@ -104,6 +104,7 @@ class PortalController extends Controller
             'pet_ID' => 'required',
             'doctor_ID' => 'required',
             'appointment_date' => 'required|date',
+            'appointment_time' => 'required',
             'purpose' => 'required',
         ]);
 
@@ -150,52 +151,4 @@ class PortalController extends Controller
     }
 
 
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
 }
